9b5bbf3a37e097b0e9dd49a7563187f5240f4917
Dmitriy <dmytro.stetsenko@student.sumdu.edu.ua>
2019-12-15 23:23:12 +0200

    added a default value

--- Hello.java
+++ Hello.java
@@ -2,6 +2,11 @@ public class Hello
 {
 	public static void main(String args[])
 	{
-		System.out.println("Hello " + args[0] + "!");
+		String name = "World";
+		if (args.length != 0)
+		{
+			name = args[0];
+		}
+		System.out.println("Hello " + name + "!");
 	}
 }
